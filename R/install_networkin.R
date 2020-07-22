57b26a7c58445ffc1ae740a832b43421f622eb9a
gaelf <fortin.gael@outlook.fr>
2020-07-22 10:58:24 +0200

    Fixed permission denied for networKIN.py

--- R/install_networkin.R
+++ R/install_networkin.R
@@ -5,5 +5,6 @@
 networkin_setup <- function(){
   message('Unzipping NetworKIN archive...')
   unzip('networkin.zip')
+  system("chmod +x networkin/bin/NetworKIN3.0_release/NetworKIN.py") # Allow NetworKIN to be executed
   message('NetworKIN setup completed.')
 }
