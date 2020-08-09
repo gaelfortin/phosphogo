2596bf98c5ca5e4e70c1868843f0fbdcf521bdbd
gaelfortin <fortin.gael@outlook.fr>
2020-08-09 13:01:56 +0200

    Fixed issue in networkin.zip unzipping

--- R/networkin_setup.R
+++ R/networkin_setup.R
@@ -7,7 +7,7 @@ networkin_setup <- function(){
   message('Downloading NetworKIN archive...')
   download.file('https://github.com/gaelfortin/phosphogo/raw/master/networkin.zip', 'networkin.zip')
   message('Unzipping NetworKIN archive...')
-  unzip(networkin_zip)
+  unzip(networkin.zip)
   system(paste0("chmod +x ", networkin_folder, "/bin/NetworKIN3.0_release/NetworKIN.py")) # Allow NetworKIN to be executed
   message('NetworKIN setup completed.')
 }
