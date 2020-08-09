1481ebc76cb670b054c33b446bc8af84feb0a21e
gaelfortin <fortin.gael@outlook.fr>
2020-08-09 12:54:25 +0200

    networkin.zip downloaded added

--- R/networkin_setup.R
+++ R/networkin_setup.R
@@ -1,16 +1,11 @@
 #' NetworKIN setup
-#' NetworKIN is properly setup with this function.
-#' @param networkin_zip `<character>` Location of the networkin.zip
-#' archive to be installed.
-#' @param networkin_folder `<character>` Location in which NetworKIN will be
-#' installed. By default, NetworKIN is installed in the R project main folder
-#' from networkin.zip (also in the main folder). For an example, 
-#' see the project architecture at https://github.com/gaelfortin/phosphogo
+#' NetworKIN is properly downloaded and installed with this function.
 #' @importFrom utils unzip
 #' @export
 #' 
-networkin_setup <- function(networkin_zip = 'networkin.zip',
-                            networkin_folder = 'networkin'){
+networkin_setup <- function(){
+  message('Downloading NetworKIN archive...')
+  download.file('https://github.com/gaelfortin/phosphogo/raw/master/networkin.zip', 'networkin.zip')
   message('Unzipping NetworKIN archive...')
   unzip(networkin_zip)
   system(paste0("chmod +x ", networkin_folder, "/bin/NetworKIN3.0_release/NetworKIN.py")) # Allow NetworKIN to be executed
