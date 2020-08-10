eaa3c800523d99bb5445076b60f201e916780867
gaelf <fortin.gael@outlook.fr>
2020-08-10 11:44:59 +0200

    Fixed unzip issue for networkin.zip

--- R/networkin_setup.R
+++ R/networkin_setup.R
@@ -7,7 +7,7 @@ networkin_setup <- function(){
   message('Downloading NetworKIN archive...')
   download.file('https://onedrive.live.com/download?cid=38F5374142AA4416&resid=38F5374142AA4416%213566&authkey=AHQSjMEM2FKJg60', 'networkin.zip')
   message('Unzipping NetworKIN archive...')
-  unzip(networkin.zip)
+  unzip('networkin.zip')
   system(paste0("chmod +x ", networkin_folder, "/bin/NetworKIN3.0_release/NetworKIN.py")) # Allow NetworKIN to be executed
   message('NetworKIN setup completed.')
 }
