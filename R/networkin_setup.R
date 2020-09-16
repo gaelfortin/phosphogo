aa84f9a4fcfe3db6fa64186e2592bb18f4ea8bd1
Gael Fortin <fortin.gael@outlook.fr>
2020-09-16 15:32:59 +0200

    Removed unzipping messages

--- R/networkin_setup.R
+++ R/networkin_setup.R
@@ -5,10 +5,11 @@
 #' @export
 #' 
 networkin_setup <- function(){
+  unlink('networkin', recursive = TRUE) #Remove old networkin folder
   message('Downloading NetworKIN archive...')
   download.file('https://onedrive.live.com/download?cid=38F5374142AA4416&resid=38F5374142AA4416%213567&authkey=AGdpgSa0BgvVV-U', 'networkin.zip')
   message('Unzipping NetworKIN archive...')
-  system('unzip networkin.zip') #use unix unzip as R unzip alters networkin files
+  system('unzip networkin.zip', ignore.stdout = TRUE) #use unix unzip as R unzip alters networkin files
   system(paste0("chmod +x networkin/bin/NetworKIN3.0_release/NetworKIN.py")) # Allow NetworKIN to be executed
   message('NetworKIN setup completed.')
 }
