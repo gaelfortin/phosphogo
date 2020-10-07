838238bee399c9a3b4e51784c31173b719920f24
gaelf <fortin.gael@outlook.fr>
2020-10-07 18:32:15 +0200

    Input details, seetup and IV-KEA implemented

--- gui/server.R
+++ gui/server.R
@@ -1,17 +1,127 @@
 library(shiny)
+library(phosphogo)
+library(tidyverse)
+library(shinyDirectoryInput) # install with devtools::install_github('wleepang/shiny-directory-input')
+# Test if NetworKIN is installed
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
+shinyServer(function(input, output, session) {
+    setwd(dir = "../")
+   
+   # Verify that NetworKIN is installed
+   output$networkin_verif <- renderText({
+       ifelse(file.exists("networkin/bin/NetworKIN3.0_release/NetworKIN.py")==TRUE, 
+              "NetworKIN is properly installed.",
+              "WARNING! NetworKIN is not installed.")})
+   
+   output$networkin_install <-
+       renderUI(expr = if (file.exists("networkin/bin/NetworKIN3.0_release/NetworKIN.py")==TRUE) { 
+           NULL } else {
+               actionButton("run_networkin_setup", label = "Install NetworKIN")})
+   
+   observeEvent(
+      input$run_networkin_setup, {
+         networkin_setup()
+         output$networkin_install <-
+            renderUI(expr = if (file.exists("networkin/bin/NetworKIN3.0_release/NetworKIN.py")==TRUE) { 
+               NULL } else {
+                  actionButton("run_networkin_setup", label = "Install NetworKIN")})
+         output$networkin_verif <- renderText({
+            ifelse(file.exists("networkin/bin/NetworKIN3.0_release/NetworKIN.py")==TRUE, 
+                   "NetworKIN is properly installed.",
+                   "WARNING! NetworKIN is not installed.")})})
+   
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+   # Verify that IV-KEA is installed
+   output$ivkea_verif <- renderText({
+      ifelse(file.exists("data/imported/invitrodb.csv")==TRUE, 
+             "IV-KEA is properly installed.",
+             "WARNING! IV-KEA is not installed.")})
+   
+   output$ivkea_install <-
+      renderUI(expr = if (file.exists("data/imported/invitrodb.csv")==TRUE) { 
+         NULL } else {
+            actionButton("run_ivkea_setup", label = "Install IV-KEA")})
+   
+   observeEvent(
+      input$run_ivkea_setup, {
+         ivkea_setup()
+         output$ivkea_install <-
+            renderUI(expr = if (file.exists("data/imported/invitrodb.csv")==TRUE) { 
+               NULL } else {
+                  actionButton("run_ivkea_setup", label = "Install IV-KEA")})
+         output$ivkea_verif <- renderText({
+            ifelse(file.exists("data/imported/invitrodb.csv")==TRUE, 
+                   "IV-KEA is properly installed.",
+                   "WARNING! IV-KEA is not installed.")})})
+   
+   # Select where to save outputs
+   observeEvent(
+      ignoreNULL = TRUE,
+      eventExpr = {
+         input$output_dir
+      },
+      handlerExpr = {
+         if (input$output_dir > 0) {
+            # condition prevents handler execution on initial app launch
+            
+            # launch the directory selection dialog with initial path read from the widget
+            output_dir <- choose.dir(default = readDirectoryInput(session, 'output_dir'))
+            
+            # update the widget value
+            updateDirectoryInput(session, 'output_dir', value = output_dir)
+         }
+      }
+   )
+   
+   observeEvent(input$run_ivkea, {
+      withProgress(message = 'Running IV-KEA', value = 0, {
+         incProgress(1/3, detail = 'Preparing data for IV-KEA')
+         networkin_input(
+            phospho_file = input$phosphofile[1,4],
+            phosphosites_column = input$phosphosite_column,
+            log2_column = input$log2_column,
+            fdr_column = input$fdr_column,
+            experiment = "shiny",
+            species = input$species
+         )
+         incProgress(2/3, detail = 'Running IV-KEA')
+         perform_ivkea(clean_phospho_file = 'data/outputs/phospho_clean_shiny.csv',
+                       invitrodb_file = 'data/imported/invitrodb.csv',
+                       experiment = 'shiny')
+         perform_Fisher_exact_test(top_predictions_file = 'data/outputs/ivkea_predictions_shiny.csv',
+                                   experiment = 'ivkea_shiny')
+         incProgress(3/3, detail = 'Generating results plots')
+         make_volcano_plot(kinase_enrichment_file = 'data/analyses/kinase_enrichment_ivkea_shiny.csv', 
+                           odds_ratio = up_vs_down_odds_ratio,
+                           FDR_cutoff = 0.05, 
+                           FDR = up_vs_down_FDR,
+                           graph_title = "Kinases prediction enrichment (IV-KEA) up vs down",
+                           x_axis_title = "log2(odds ratio)",
+                           save_path = "figures/up_down_volcano_plot_shiny_ivkea.pdf")
+         make_volcano_plot(kinase_enrichment_file = 'data/analyses/kinase_enrichment_ivkea_shiny.csv', 
+                           odds_ratio = up_vs_tot_odds_ratio,
+                           FDR_cutoff = 0.05, 
+                           FDR = up_vs_tot_FDR,
+                           graph_title = "Kinases prediction enrichment (IV-KEA) up vs tot",
+                           x_axis_title = "log2(odds ratio)",
+                           save_path = "figures/up_tot_volcano_plot_ivkea_shiny.pdf")
+         make_volcano_plot(kinase_enrichment_file = 'data/analyses/kinase_enrichment_ivkea_shiny.csv', 
+                           odds_ratio = down_vs_tot_odds_ratio,
+                           FDR_cutoff = 0.05, 
+                           FDR = down_vs_tot_FDR,
+                           graph_title = "Kinases prediction enrichment (IV-KEA) down vs tot",
+                           x_axis_title = "log2(odds ratio)",
+                           save_path = "figures/down_tot_volcano_plot_ivkea_shiny.pdf")
+         })
+      
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
+      
+      
+      
+      
+      
+      
+   })
+   
 })
--- gui/ui.R
+++ gui/ui.R
@@ -1,24 +1,30 @@
 library(shiny)
+library(shinyDirectoryInput)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+    titlePanel("Setup and input"),
+    
+    textOutput("networkin_verif"),
+    
+    uiOutput('networkin_install'),
+    
+    textOutput("ivkea_verif"),
+    
+    uiOutput('ivkea_install'),
+    
+    fileInput("phosphofile", label = h3("Phosphoproteomic input"), placeholder = "Select a .csv or .xlsx file"),
+    textInput("phosphosite_column", label = h3("Enter the name of the phosphosite colum"), value = "PhosphoSite"),
+    textInput("log2_column", label = h3("Enter the name of the Log2 colum"), value = "Log2"),
+    textInput("fdr_column", label = h3("Enter the name of the FDR colum"), value = "Adj. Pvalue"),
+    radioButtons("species", label = h3("Select a species"),
+                 choices = list("Human" = "hsa", "Mouse" = "mmu"), 
+                 selected = "hsa"),
+    directoryInput('output_dir', label = 'Select a directory to save all outputs', value = '~'),
+    
+    
+    actionButton(inputId = "run_ivkea", label = "Run IV-KEA")
     )
-))
+)
